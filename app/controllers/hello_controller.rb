0fd0f00c4b61a478c7fd3f4bb9fb32dbe93966ea
yuki-nakagawa77 <tuna27kan789@gmail.com>
2021-01-19 20:36:13 +0900

    クエリーパラメータの利用

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,22 @@
 class HelloController < ApplicationController
+
+
   def index
-    msg = '
+    if params['msg'] != nil then
+      msg = 'Hello, ' + params['msg'] + '!'
+    else 
+      msg = 'this is sample page.'
+    end
+    html = '
     <html>
     <body>
       <h1>Sample Page</h1>
-      <p>this is Rails sample page!</p>
+      <p>' + msg + '</p>
     </body>
     </html>
     '
-    render html: msg.html_safe
+    render html: html.html_safe
   end
+
+
 end
