b66d43e7c1dd495089fefa70dbf9bddf4abf477e
yuki-nakagawa77 <tuna27kan789@gmail.com>
2021-01-21 16:02:58 +0900

    フォームの送信

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,16 +2,16 @@ class HelloController < ApplicationController
 
 
   def index
-    if params['msg'] != nil then
-      @title = params['msg']
+    if request.post? then
+      @title = 'Result'
+      @msg = 'you typed: ' + params['input1'] + '.'
+      @value = params['input1']
     else
-      @title = 'index'
+      @title = 'Index'
+      @msg = 'type text...'
+      @value = ''
     end
-    @msg ='this is redirect sample...'
   end
 
 
-  def other
-    redirect_to action: :index, params: {'msg': 'from other page'}
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
+  post 'hello', to: 'hello#index'
+  post 'hello/index'
 end
