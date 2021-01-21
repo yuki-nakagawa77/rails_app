745dc668f1a5f092594942b8bc15168d5357ac3f
yuki-nakagawa77 <tuna27kan789@gmail.com>
2021-01-21 15:48:58 +0900

    Bootstrap使用

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,20 +2,6 @@ class HelloController < ApplicationController
 
 
   def index
-    if params['msg'] != nil then
-      msg = 'Hello, ' + params['msg'] + '!'
-    else 
-      msg = 'this is sample page.'
-    end
-    html = '
-    <html>
-    <body>
-      <h1>Sample Page</h1>
-      <p>' + msg + '</p>
-    </body>
-    </html>
-    '
-    render html: html.html_safe
   end
 
 
