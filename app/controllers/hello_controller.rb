0f658fd94ac197b4603552944b2912705853afcd
yuki-nakagawa77 <tuna27kan789@gmail.com>
2021-01-23 21:48:19 +0900

    CSRF対策を通過

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,17 +1,18 @@
 class HelloController < ApplicationController
+  protect_from_forgery
 
 
-  def index
-    if request.post? then
-      @title = 'Result'
-      @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params['input1']
-    else
-      @title = 'Index'
-      @msg = 'type text...'
-      @value = ''
-    end
-  end
+ def index
+   if request.post? then
+     @title = 'Result'
+     @msg = 'you typed: ' + params['input1'] + '.'
+     @value = params['input1']
+   else
+     @title = 'Index'
+     @msg = 'type text...'
+     @value = ''
+   end
+ end
 
 
 end
