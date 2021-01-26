ca08de9dcf911e42ec2923fb958f54d6f84650c5
yuki-nakagawa77 <tuna27kan789@gmail.com>
2021-01-26 21:50:54 +0900

    選択リスト作成

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -4,14 +4,14 @@ class HelloController < ApplicationController
   def index
     if request.post? then
       @title = 'Result'
-      if params['r1'] then
-        @msg = 'you selected: ' + params['r1']
+      if params['s1'] then
+        @msg = 'you selected: '+ params['s1']
       else 
         @msg = 'not selected...'
       end
     else
       @title = 'Index'
-      @msg = 'select radio button...'
+      @msg = 'select List...'
     end
   end
 
